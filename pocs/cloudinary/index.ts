0d5c0978e4d61c679da0c3e078f41105f31cd0ab
Bruno Melo <bruno94@outlook.com>
2023-10-05 18:30:32 -0300

    remove chokidar

--- pocs/cloudinary/index.ts
+++ pocs/cloudinary/index.ts
@@ -1,6 +1,5 @@
 import { v2 as cloudinary } from "cloudinary";
 import path from "path";
-import chokidar from "chokidar";
 
 const SCRET = "";
 const KEY = "";
@@ -12,22 +11,7 @@ cloudinary.config({
   api_secret: SCRET,
 });
 const folder = path.resolve(__dirname, "img");
-const watcher = chokidar.watch(folder);
 const log = console.log.bind(console);
-
-watcher
-  .on("add", (path) =>
-    cloudinary.uploader
-      .upload(path, {
-        folder: "production",
-      })
-      .then((result) => console.log(result))
-  )
-  .on("unlink", (path) => log(`File ${path} has been removed`))
-  .on("raw", (event, path, details) => {
-    // internal
-    log("Raw event info:", event, path, details);
-  });
 
 // cloudinary.uploader
 //   .upload(img, {
